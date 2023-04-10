2c32b7994b35798ce22d8869789e10f3067672d9
Fatma <fmutlu3002@gmail.com>
2023-04-10 17:31:00 +0300

    Generic Repository Pattern

---
+++ MyBlog.Data/Repositories/Abstaction/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using MyBlog.Core.Entities;
+
+namespace MyBlog.Data.Repositories.Abstaction
+{
+    public interface IRepository<T> where T: BaseEntity
+    {
+        Task<T> AddAysnc();
+    }
+}
---
+++ MyBlog.Data/Repositories/Concretes/Repository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Core.Entities;
+using MyBlog.Data.Context;
+
+namespace MyBlog.Data.Repositories.Concretes
+{
+    public class Repository<T> where T: BaseEntity
+    {
+        private readonly MyBlogDbContext context;
+
+        public Repository(MyBlogDbContext context)
+        {
+            this.context = context;
+        }
+
+        private DbSet<T> Table { get => context.Set<T>(); }
+
+        public async Task<T> AddAysnc(T model)
+        {
+            await Table.AddAsync(model);
+            return model;
+        }
+    }
+}
