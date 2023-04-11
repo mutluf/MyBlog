d4514f093012f67e38ad72646fe6ccfc2e1b1a44
Fatma <fmutlu3002@gmail.com>
2023-04-11 17:11:47 +0300

    Unit Of Work Pattern Concrete

--- MyBlog.Data/UnitOfWorks/UnitOfWork.cs
+++ MyBlog.Data/UnitOfWorks/UnitOfWork.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MyBlog.Core.Entities;
+using MyBlog.Data.Context;
+using MyBlog.Data.Repositories.Abstaction;
+using MyBlog.Data.Repositories.Concretes;
 
 namespace MyBlog.Data.UnitOfWorks
 {
-    internal class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly MyBlogDbContext _context;
+
+        public UnitOfWork(MyBlogDbContext context)
+        {
+            _context = context;
+        }
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public IRepository<T> GetRepository<T>() where T : BaseEntity
+        {
+            return new Repository<T>(_context);
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
     }
 }
