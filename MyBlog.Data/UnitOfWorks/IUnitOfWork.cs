edaca1d47fc295e14162b0047c578196087ac1d0
Fatma <fmutlu3002@gmail.com>
2023-04-11 17:09:38 +0300

    fix

--- MyBlog.Data/UnitOfWorks/IUnitOfWork.cs
+++ MyBlog.Data/UnitOfWorks/IUnitOfWork.cs
@@ -1,10 +1,5 @@
 ﻿using MyBlog.Core.Entities;
 using MyBlog.Data.Repositories.Abstaction;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyBlog.Data.UnitOfWorks
 {
